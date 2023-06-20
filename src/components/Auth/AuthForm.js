ba10cd4fd47f824530d777ba5b38bc15aa88c676
shivam2399 <dubey.shivam2399@gmail.com>
2023-06-20 15:41:39 +0530

    implementing login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -20,36 +20,44 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     setIsLoading(true);
+    let url;
     if(isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCdrqv7Tcd4pp_JRBqNAuKhAvCLjxPZWZc'
     } else {
-      fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCdrqv7Tcd4pp_JRBqNAuKhAvCLjxPZWZc',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
-      ).then((res) => {
-        setIsLoading(false);
-        if(res.ok) {
-
-        } else {
-          return res.json().then((data) => {
-            let errorMessage = 'Authentication faled';
-            if(data && data.error && data.error.message) {
-              errorMessage = data.error.message
-            }
-            alert(errorMessage);
-          })
-        }
-      })
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCdrqv7Tcd4pp_JRBqNAuKhAvCLjxPZWZc'
     }
+    fetch(url,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    ).then((res) => {
+      setIsLoading(false);
+      if(res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = 'Authentication faled';
+          if(data && data.error && data.error.message) {
+            errorMessage = data.error.message
+          }
+          throw new Error(errorMessage);
+        })
+      }
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      alert(err.message);
+    })
   }
 
   return (
