77267c3037ae792a22fd172ee36317b0c57ddcc4
shivam2399 <dubey.shivam2399@gmail.com>
2023-07-02 13:39:58 +0530

    auto logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,41 +1,55 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    token: '',
-    isLoggedIn: false,
-    login: (token) => {},
-    logout: () => {}
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
 });
 
 export const AuthContextProvider = (props) => {
-    const initialToken = localStorage.getItem('token')
-    const [token, setToken] = useState(initialToken);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
+  const [logoutTimeout, setLogoutTimeout] = useState(null);
 
-    const userIsLoggedIn = !!token;
+  const userIsLoggedIn = !!token;
 
-    const loginHandler = (token) => {
-        setToken(token);
-        localStorage.setItem('token', token);
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+    const timeout = setTimeout(logoutHandler, 5 * 60 * 1000);
+    setLogoutTimeout(timeout);
+  };
+
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+    if (logoutTimeout) {
+      clearTimeout(logoutTimeout);
+      setLogoutTimeout(null);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (logoutTimeout) {
+        clearTimeout(logoutTimeout);
+      }
     };
+  }, [logoutTimeout]);
 
-    const logoutHandler = () => {
-        setToken(null)
-        localStorage.removeItem('token');
-    }
+  const contextValue = {
+    token: token,
+    isLoggedIn: userIsLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
 
-    const contextValue = {
-        token: token,
-        isLoggedIn: userIsLoggedIn,
-        login: loginHandler,
-        logout: logoutHandler
-    }
-
-
-    return (
-        <AuthContext.Provider value={contextValue}>
-           {props.children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
